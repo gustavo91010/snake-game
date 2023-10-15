d42d30e7f6936e370da70bf1377a3554f26eef35
gustavo91010 <gustavo910@gmail.com>
2023-10-15 01:14:12 -0300

    feat: end game ok

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 const canvas = document.querySelector("canvas");
 const contexto = canvas.getContext("2d");
+const initialPosition= { x: 300, y: 300 }
 
+const scoreValue= document.querySelector('.score__value')
+const score= document.querySelector('.score')
+
+const finalScoreValue= document.querySelector('.menu__screen__final_score > span')
+const menu= document.querySelector('.menu__screen')
+const buttonPlay= document.querySelector('.menu__screen__jogar_novamente')
 /**
  * 
 contexto.fillStyle= "red" // estilo do preenchimento da figura
 contexto.fillRect(300, 300, 10, 10)// um retantugo nas cordenadas x e y com altura e comprimento de...
 */
+let endGame= false;
 let direction = ""
 let loopId
+let time=300
 
-const audio= new Audio('../assets/audio.mp3')
+// Score:
+const incrementScore= ()=>{
+    scoreValue.innerText= Number(scoreValue.innerText) + 10
+}
+
+const audio = new Audio('../assets/audio.mp3')
 const size = 30;
-const snake = [
-    { x: 0, y: 0 },
-
+let snake = [
+    initialPosition,
     /*
-    { x: 200, y: 200 },
-    { x: 230, y: 200 },
-    { x: 260, y: 200 },
+    { x: 0, y: 30 },
+    { x: 0, y: 60 },
+    { x: 0, y: 90 },
+    { x: 0, y: 120 },
+    { x: 0, y: 150 },
+    { x: 0, y: 180 },
+
+  
 
     A cobrinha será um array de posições, pois cada objeto tera a posição x e y*/
 ]
@@ -48,8 +66,20 @@ const food = {
     color: colorAtual
 }
 
+ const aumentoDificuldade= ()=>{
+    if(time > 200){
+        time= time -5
+    }
+    if(time< 120){
+        time= time -3
 
+    }
+    if(time< 100 && time > 50){
+        time= time -1
 
+    }
+ }
+// Responsavel por comer as frutunhas
 const chackEat = () => {
     // Se a posição da cabeça for igual ao da comida, quer dizer que a cobra comeu a comida
     const head = snake[snake.length - 1];
@@ -57,10 +87,13 @@ const chackEat = () => {
 
         snake.push(head)
         audio.play()
+        incrementScore()
+        aumentoDificuldade()
+      //  time= time-10
 
         // Gerando uma nova comida:
         colorAtual = randomColor()
-        
+
 
         // Verificando se a comida vai ser gerada em alguma posição ja ocupada pela cobra:
         let x = randomPosition();
@@ -69,16 +102,55 @@ const chackEat = () => {
         // esse find, ta mais para um filter, ele ta pegando todas as posições da cobrinha e verificando
         // se tem alguem com a posição x e y iguais a que acabei de gerar, se sim, manda um true e entra no while...
         // se achar, ele gera novamente o y e x
-        while (snake.find((position) =>position.x == x && position.y == y)){
-             x = randomPosition();
-             y = randomPosition();
+        while (snake.find((position) => position.x == x && position.y == y)) {
+            x = randomPosition();
+            y = randomPosition();
         }
-        food.x= x;
+        food.x = x;
         food.y = y;
         food.color = colorAtual
 
     }
 }
+
+
+const gameOver=()=>{
+    endGame= true
+    direction= undefined
+    menu.style.display= "flex"
+    scoreValue.style.display= "none"
+    score.style.display= "none"
+
+    finalScoreValue.innerText= scoreValue.innerText
+
+    canvas.style.filter= "blur(2px)" // deixar o elemento disfocado quando perder... adicionar o z-index no elemento que deveria continuar aparecebdo, no caos, no menu
+    snake=[initialPosition ]
+  
+   time= 300
+}
+// Responsavel por verificar as colisões
+const checkCollision = () => {
+
+    const canvasLimit = canvas.width - size;
+    const head = snake[snake.length - 1];
+    const pescoco = snake.length-2;
+
+    const wallCollision = head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+    
+    const selfCollision= snake.find((position, index)=>{
+        // se  posição da cobra for alguma posição contendo abaixo da cabeça, deve dar erro.
+        return index< pescoco && position.x== head.x && position.y == head.y
+    })
+
+    if (wallCollision || selfCollision) {
+        //alert("Você perdeu")
+        gameOver()
+    }
+
+}
+
+
+
 
 
 const drawSnake = () => {
@@ -218,10 +290,11 @@ const gameloop = () => {
     moveSnake();
     drawSnake();
     chackEat();
+    checkCollision()
 
     loopId = setTimeout(() => {
         gameloop()
-    }, 300)
+    }, time)
     // setTimeout: determina o que ele faz e o intervalo de tempo
 
 }
@@ -249,8 +322,9 @@ setInterval(() => {
 document.addEventListener("keydown", ({ key }) => {
     //console.log(key.replace('Arrow',""))
 
+if(!endGame){
 
-
+    
     if (key == "ArrowRight" && direction != "Left") {
         // direction= key.replace('Arrow',"")
         direction = "Right"
@@ -268,10 +342,22 @@ document.addEventListener("keydown", ({ key }) => {
 
     }
 
+    }
 
 
 
 
+})
+buttonPlay.addEventListener("click", ()=>{
+    endGame= false
+    menu.style.display= "none"
+    
+    
+    scoreValue.style.display= "flex"
+    score.style.display= "flex"
+    canvas.style.filter= "none" 
+    scoreValue.innerText= "00";
+    
 })
 
 // 
